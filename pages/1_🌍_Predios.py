48407fce1e83263e1ed0531fd1406100a8925806
jennychaux-eng <jenny.chaux@uao.edu.co>
2026-05-18 12:24:15 -0500

    Update 1_🌍_Predios.py

--- pages/1_🌍_Predios.py
+++ pages/1_🌍_Predios.py
@@ -1,1 +1,19 @@
+import streamlit as st
 
+st.title("🌍 Predios")
+
+st.markdown("## Horizontes")
+
+st.info("Bosque seco tropical")
+
+st.metric("Área total", "12 ha")
+st.metric("Altitud", "1450 msnm")
+
+st.markdown("---")
+
+st.markdown("## Lomas de Dapa")
+
+st.info("Bosque de niebla")
+
+st.metric("Área total", "18 ha")
+st.metric("Altitud", "1850 msnm")
