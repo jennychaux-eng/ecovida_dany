1298ceea5e3131724613ee35fc63782038a43c08
jennychaux-eng <jenny.chaux@uao.edu.co>
2026-05-18 12:25:10 -0500

    Add Streamlit interface for tree data input

--- pages/3_🌳_Individuos.py
+++ pages/3_🌳_Individuos.py
@@ -1,1 +1,15 @@
+import streamlit as st
 
+st.title("🌳 Individuos Arbóreos")
+
+codigo = st.text_input("Código del árbol")
+especie = st.text_input("Especie")
+
+st.file_uploader(
+    "Subir fotografía",
+    type=["png", "jpg", "jpeg"]
+)
+
+st.text_area("Observaciones")
+
+st.button("Guardar")
